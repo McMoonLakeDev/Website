e5a2c5c83bce292bd825cccf9ba3ba937e7bc62e
nihuge <2944423432@qq.com>
2016-10-14 17:35:14 +0800

    Initial commit

---
+++ class/website.class.php
@@ -0,0 +1,14 @@
+<?php
+   require "userinfo.class.php";
+    class website extends userdo
+    {
+        public function _counster(){
+
+
+
+        }
+
+
+
+
+    }
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/10/14
+ * Time: 17:27
+ */
