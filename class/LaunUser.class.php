be0673dffe43f9b6795233c52b1654b25406f2aa
nihuge <2944423432@qq.com>
2016-10-18 09:07:14 +0800

    admin

--- class/LaunUser.class.php
+++ class/LaunUser.class.php
@@ -81,25 +81,7 @@ class LaunUser
             );
         }
         return json_encode($msg);
+        exit();
         }
-
-
-    function get_onlineip()
-    {
-        $onlineip = '';
-        if (getenv('HTTP_CLIENT_IP') && strcasecmp(getenv('HTTP_CLIENT_IP'), 'unknown')) {
-            $onlineip = getenv('HTTP_CLIENT_IP');
-        } elseif (getenv('HTTP_X_FORWARDED_FOR') && strcasecmp(getenv('HTTP_X_FORWARDED_FOR'), 'unknown')) {
-            $onlineip = getenv('HTTP_X_FORWARDED_FOR');
-        } elseif (getenv('REMOTE_ADDR') && strcasecmp(getenv('REMOTE_ADDR'), 'unknown')) {
-            $onlineip = getenv('REMOTE_ADDR');
-        } elseif (isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], 'unknown')) {
-            $onlineip = $_SERVER['REMOTE_ADDR'];
-        }
-        if ($onlineip == "::1") {
-            $onlineip = "127.0.0.1";
-        }
-        return $onlineip;
-    }
 
 }
--- class/userinfo.class.php
+++ class/userinfo.class.php
@@ -1,22 +1,24 @@
 <?php
 session_start();
-require "sql.class.php";
-class Userdo extends Sql{
+require "../includes/db.class.php";
+class UserDo extends DB
+{
     /*
      * 获取真实IP地址，无视代理
      */
-    function get_onlineip() {
+    function get_onlineip()
+    {
         $onlineip = '';
-        if(getenv('HTTP_CLIENT_IP') && strcasecmp(getenv('HTTP_CLIENT_IP'), 'unknown')) {
+        if (getenv('HTTP_CLIENT_IP') && strcasecmp(getenv('HTTP_CLIENT_IP'), 'unknown')) {
             $onlineip = getenv('HTTP_CLIENT_IP');
-        } elseif(getenv('HTTP_X_FORWARDED_FOR') && strcasecmp(getenv('HTTP_X_FORWARDED_FOR'), 'unknown')) {
+        } elseif (getenv('HTTP_X_FORWARDED_FOR') && strcasecmp(getenv('HTTP_X_FORWARDED_FOR'), 'unknown')) {
             $onlineip = getenv('HTTP_X_FORWARDED_FOR');
-        } elseif(getenv('REMOTE_ADDR') && strcasecmp(getenv('REMOTE_ADDR'), 'unknown')) {
+        } elseif (getenv('REMOTE_ADDR') && strcasecmp(getenv('REMOTE_ADDR'), 'unknown')) {
             $onlineip = getenv('REMOTE_ADDR');
-        } elseif(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], 'unknown')) {
+        } elseif (isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], 'unknown')) {
             $onlineip = $_SERVER['REMOTE_ADDR'];
         }
-        if($onlineip == "::1"){
+        if ($onlineip == "::1") {
             $onlineip = "127.0.0.1";
         }
         return $onlineip;
@@ -27,7 +29,8 @@ class Userdo extends Sql{
      * 如果秘钥太长，解密时会报错
      */
 
-    function en_cookie($data, $key) {
+    function en_cookie($data, $key)
+    {
         $prep_code = serialize($data);
         $block = mcrypt_get_block_size('des', 'ecb');
         if (($pad = $block - (strlen($prep_code) % $block)) < $block) {
@@ -36,11 +39,13 @@ class Userdo extends Sql{
         $encrypt = mcrypt_encrypt(MCRYPT_DES, $key, $prep_code, MCRYPT_MODE_ECB);
         return base64_encode($encrypt);
     }
+
     /*
      * 稀有cookie 解密
      * 如果秘钥太长，解密时会报错
      */
-    function de_cookie($str, $key) {
+    function de_cookie($str, $key)
+    {
         $str = base64_decode($str);
         $str = mcrypt_decrypt(MCRYPT_DES, $key, $str, MCRYPT_MODE_ECB);
         $block = mcrypt_get_block_size('des', 'ecb');
@@ -50,48 +55,52 @@ class Userdo extends Sql{
         }
         return unserialize($str);
     }
-    function pubsession($username,$password){
+
+
+    function pubsession($username, $password)
+    {
         $name = mysql_escape_string($username);
-        $names = $this->query("Select * from ecm_member where user_name='{$username}' and password='{$password}'");
-        $namen = mysql_num_rows($names);
-        if($namen <> 0){
-                $cookie = array(
-                    "username" => $name,
-                    "password" => $password,
-                    "last_ip" => $this->get_onlineip()
-                );
+//        $names = $this->query("Select * from ecm_member where user_name='{$username}' and password='{$password}'");
+        $namen = $this->get_row("Select * from ecm_member where user_name='{$username}' and password='{$password}'");
+        if ($namen <> 0) {
+            $cookie = array(
+                "username" => $name,
+                "password" => $password,
+                "last_ip" => $this->get_onlineip()
+            );
             $userinfo = $this->user_info($username);
-                $session = array(
-                    'user_id' => $userinfo['user_id'],
-                    'user_name' => $userinfo['user_name'],
-                    'real_name' => $userinfo['real_name'],
-                    'reg_time' => $userinfo['reg_time'],
-                    'last_login' =>$userinfo['last_login'],
-                    'last_ip' => $userinfo['last_ip'],
-                    'store_id' => $userinfo['store_id'],
-                    'belong_store_id' => $userinfo['belong_store_id'],
-                    'roles' => $userinfo['roles']
-                );
-                $encookie = $this->en_cookie($cookie,"shop");
-                setcookie("userinfo",$encookie,time()+3600*24);
-                $_SESSION['user_info'] = $session;
-        $this->query("Update ecm_member set last_ip='{$cookie['last_ip']}' Where user_name = '{$username}'");
+            $session = array(
+                'user_id' => $userinfo['user_id'],
+                'user_name' => $userinfo['user_name'],
+                'real_name' => $userinfo['real_name'],
+                'reg_time' => $userinfo['reg_time'],
+                'last_login' => $userinfo['last_login'],
+                'last_ip' => $userinfo['last_ip'],
+                'store_id' => $userinfo['store_id'],
+                'belong_store_id' => $userinfo['belong_store_id'],
+                'roles' => $userinfo['roles']
+            );
+            $encookie = $this->en_cookie($cookie, "shop");
+            setcookie("userinfo", $encookie, time() + 3600 * 24);
+            $_SESSION['user_info'] = $session;
+            $this->query("Update ecm_member set last_ip='{$cookie['last_ip']}' Where user_name = '{$username}'");
             $msg = '{"success":true,"msg":"登录成功！"}';
-        }else{
+        } else {
             $msg = '{"success":false,"msg":"登录失败！"}';
         }
         return $msg;
     }
+
     /**
      *  设置COOKIE
      *
-     *  @access public
-     *  @param  string $key     要设置的COOKIE键名
-     *  @param  string $value   键名对应的值
-     *  @param  int    $expire  过期时间
-     *  @return void
+     * @access public
+     * @param  string $key 要设置的COOKIE键名
+     * @param  string $value 键名对应的值
+     * @param  int $expire 过期时间
+     * @return void
      */
-    function ecm_setcookie($key, $value, $expire = 0, $cookie_path=COOKIE_PATH, $cookie_domain=COOKIE_DOMAIN)
+    function ecm_setcookie($key, $value, $expire = 0, $cookie_path = COOKIE_PATH, $cookie_domain = COOKIE_DOMAIN)
     {
         setcookie($key, $value, $expire, $cookie_path, $cookie_domain);
     }
@@ -99,12 +108,65 @@ class Userdo extends Sql{
     /**
      *  获取COOKIE的值
      *
-     *  @access public
-     *  @param  string $key    为空时将返回所有COOKIE
-     *  @return mixed
+     * @access public
+     * @param  string $key 为空时将返回所有COOKIE
+     * @return mixed
      */
     function ecm_getcookie($key = '')
     {
         return isset($_COOKIE[$key]) ? $_COOKIE[$key] : 0;
+    }
+    /*
+     * 获取用户信息
+     *
+     */
+    function goods_id_info($goods_id){
+        $sqla = $this->query("Select goods_name,default_image from ecm_goods where goods_id='{$goods_id}'");
+        $sqlb = $this->query("Select * from ecm_goods_spec where goods_id='{$goods_id}'");
+        $sqlname = mysql_fetch_assoc($sqlb);
+        $sqlinfo = mysql_fetch_assoc($sqla);
+        $info = array_merge_recursive($sqlname,$sqlinfo);
+        return $info;
+    }
+    /*
+     *登录
+     * @author nihuge
+     */
+    function login($UserName, $PassWord)
+    {
+        $name = mysql_escape_string($UserName);
+//        $names = $this->query("Select * from ecm_member where user_name='{$username}' and password='{$password}'");
+        $namen = $this->get_row("Select * from ecm_member where user_name='{$UserName}' and password='{$PassWord}'");
+        if ($namen <> 0) {
+            $cookie = array(
+                "username" => $name,
+                "password" => $PassWord,
+                "last_ip" => $this->get_onlineip()
+            );
+            $userinfo = $this->user_info($name);
+            $session = array(
+                'user_id' => $userinfo['user_id'],
+                'user_name' => $userinfo['user_name'],
+                'real_name' => $userinfo['real_name'],
+                'reg_time' => $userinfo['reg_time'],
+                'last_login' => $userinfo['last_login'],
+                'last_ip' => $userinfo['last_ip'],
+                'store_id' => $userinfo['store_id'],
+                'belong_store_id' => $userinfo['belong_store_id'],
+                'roles' => $userinfo['roles']
+            );
+            $encookie = $this->en_cookie($cookie, "shop");
+            setcookie("userinfo", $encookie, time() + 3600 * 24);
+            $_SESSION['user_info'] = $session;
+        }
+    }
+
+    /*
+     * 获取用户信息
+     * @author nihuge
+     */
+    function user_info($name){
+        $user_info = $this->get_row("Select * from user where user_name='{$name}'");
+        return $user_info;
     }
 }
